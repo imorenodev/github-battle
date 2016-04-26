b5835e291ae2b7a9162c88a286c406b9b6177d04
imorenodev@gmail.com <imorenodev@gmail.com>
2016-04-25 23:12:28 -0400

    add ReactCSSTransition and refactor components

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -4,15 +4,13 @@ var PropTypes = React.PropTypes;
 var styles = require('../styles');
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
-
-function printAll(object) {
-  return <pre>{JSON.stringify(object, null, ' ')}</pre>;
-}
+var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 var Battle = function(props) {
   return ( props.isLoading === true
-    ? <p>LOADING..</p>
-    : <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+    ? <Loading speed={500} text='Wait One Moment' />
+    : <MainContainer> 
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
           <UserDetailsWrapper header='Player One'>
@@ -37,7 +35,7 @@ var Battle = function(props) {
             </Link>
           </div>
         </div>
-      </div>
+      </MainContainer>
   );
 }
 
